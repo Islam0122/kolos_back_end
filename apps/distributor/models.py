377f7d9fbc63628333eb6b5927ccdaf4bcd46023
Baiel1208 <baiel000005@gmail.com>
2023-10-08 18:07:46 +0600

    настройки дистрибьютора. Возможность добавить, редакти́ровать, архивировать

--- apps/distributor/models.py
+++ apps/distributor/models.py
@@ -1,3 +1,81 @@
 from django.db import models
 
 # Create your models here.
+class Distributor(models.Model):
+    photo = models.ImageField(
+        blank=False,
+        null=False,
+        verbose_name='Фотография'
+    )
+    name = models.CharField(
+        max_length=50, 
+        blank=False,
+        null=False, 
+        verbose_name='ФИО',
+    )
+    region = models.CharField(
+        max_length=150, 
+        blank=False,
+        null=False,
+        verbose_name='Регион'
+    )
+    inn = models.IntegerField(
+        blank=False,
+        unique=True,
+        null=False,
+        verbose_name='ИНН'
+    )
+    registration_address = models.CharField(
+        max_length=150, 
+        blank=False,
+        null=False,
+        verbose_name='Адрес по прописке'
+    )
+    actual_place_of_residence = models.CharField(
+        max_length=255,
+        blank=False, 
+        null=False,
+        verbose_name='Фактическое место жительства'
+    )
+    passport_series = models.IntegerField(
+        blank=False,
+        null=False,
+        unique=True,
+        verbose_name='Серия паспорта'
+    )
+    passport_id = models.IntegerField(
+        blank=False,
+        null=False,
+        verbose_name='Номер паспорта'
+    )
+    issued_by = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False,
+        verbose_name='Кем выдан'
+    )
+    date_of_issue = models.DateField(
+        null=False,
+        blank=False,
+        verbose_name='Дата выдачи'
+    )
+    validity = models.DateField(
+        null=False,
+        blank=False,
+        verbose_name='Срок действия' #нада сделать выбор даты
+    )
+    contact1 = models.IntegerField(
+        null=False,
+        blank=False,
+        verbose_name='Контактный номер один'
+    )
+    contact2 = models.IntegerField(
+        null=False,
+        blank=False,
+        verbose_name='Контактный номер два'
+    )
+    is_archived = models.BooleanField(
+        default=False,
+        verbose_name='В архив'
+    )
+
--- apps/distributor/views.py
+++ apps/distributor/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.generics import mixins
+from rest_framework import generics
+from rest_framework.viewsets import GenericViewSet
+from .models import Distributor
+from .serializers import DistributorSerializer
+
 
 # Create your views here.
+class DistributorView(mixins.ListModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.CreateModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  GenericViewSet):
+    queryset = Distributor.objects.all()
+    serializer_class = DistributorSerializer
+
+
+class DistributorDetailView(generics.RetrieveUpdateAPIView):
+    queryset = Distributor.objects.all()
+    serializer_class = DistributorSerializer
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_archived = True
+        instance.save()
+        return super().update(request, *args, **kwargs)
