c7c64a2af64c1a3b36802e3e5a2ad1eff384ecdd
islam duishobaev <dujsobaevislam01@gmail.com>
2023-10-08 22:14:11 +0600

    commit 2 -> create product and update product

--- apps/product/models.py
+++ apps/product/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    #create_data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+class Product(models.Model):
+    STATUS_CHOICES = (
+
+    ('НОРМА', 'НОРМА'),
+    ('брак', 'Брак'),
+    ('просроченный', 'Просроченный'),
+    )
+
+
+    title = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) #Категория
+    identification_number = models.AutoField(primary_key=True)
+    unit_of_measurement = models.CharField(max_length=10 , default="литр")
+    quantity = models.IntegerField(default=1)
+    price = models.IntegerField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='НОРМА')
+    @property
+    def category_title(self):
+        try:
+            return self.category.title
+        except:
+            return None
+    def __str__(self):
+        return self.title
--- apps/product/views.py
+++ apps/product/views.py
@@ -1,3 +1,30 @@
+import rest_framework.serializers
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from apps.product.serializer import ProductSerializers, ProductValidateSerializer
+from apps.product.models import Product
+
 
 # Create your views here.
+class ProductViewSet(ModelViewSet):  # GET/PUT/DELETE/CREATE/POST
+    queryset = Product.objects.all().select_related('category')
+    serializer_class = ProductSerializers
+    lookup_field = 'pk'
+
+    def create(self, request, *args, **kwargs):
+        serializer = ProductValidateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        product = Product.objects.create(
+            title=serializer.validated_data.get('title'),
+            category_id=serializer.validated_data.get('category_id'),
+            unit_of_measurement=serializer.validated_data.get('unit_of_measurement'),
+            quantity=serializer.validated_data.get('quantity'),
+            price=serializer.validated_data.get('price'),
+            status=serializer.validated_data.get('status'),
+        )
+
+        return Response(data=self.serializer_class(product, many=False).data, status=status.HTTP_201_CREATED)
