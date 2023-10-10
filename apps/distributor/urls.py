8f202cfe86e3eb6cc8c6f3c1b3a3a3fd5283adf3
Baiel1208 <baiel000005@gmail.com>
2023-10-10 16:20:17 +0600

    настройки архива

--- apps/distributor/urls.py
+++ apps/distributor/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register('distributor', views.DistributorView, 'api_distributor')
+router.register('distributor', views.DistributorViewSet, 'api_distributor')
 
 
 urlpatterns = router.urls
