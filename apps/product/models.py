a8308decc50875444d63c17d30ef5ce7f7dbbd4f
Baiel1208 <baiel000005@gmail.com>
2023-10-11 17:04:56 +0600

    перевод моделей на русский

--- apps/product/models.py
+++ apps/product/models.py
@@ -10,42 +10,105 @@ STATUS_CHOICES = (
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(
+        max_length=100,
+    )
     #create_data = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+
 class Product(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Наименование'
+    )
+    category = models.ForeignKey(Category, 
+        on_delete=models.CASCADE,
+        verbose_name='Категория'
+    ) #Категория
+    identification_number = models.AutoField(
+        primary_key=True, 
+        verbose_name='Идентификационный номер'
+    )
+    unit_of_measurement = models.CharField(
+        max_length=10, 
+        default="литр", 
+        verbose_name='Единица измерения'
+    )
+    quantity = models.IntegerField(
+        default=1, 
+        verbose_name='Количество'
+    )
+    price = models.IntegerField(
+        verbose_name='Цена'
+    )
+    status = models.CharField(
+        max_length=20, 
+        choices=STATUS_CHOICES, 
+        default='НОРМА'
+    )
 
 
-    title = models.CharField(max_length=200)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE) #Категория
-    identification_number = models.AutoField(primary_key=True)
-    unit_of_measurement = models.CharField(max_length=10 , default="литр")
-    quantity = models.IntegerField(default=1)
-    price = models.IntegerField()
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='НОРМА')
     def total_price(self):
         # Рассчитываем сумму между ценой и количеством
         return self.price * self.quantity
+
+
     @property
     def category_title(self):
         try:
             return self.category.title
         except:
             return None
+
+
     def __str__(self):
         return self.title
+    
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
+
+
 class ArchiveProduct(models.Model):
-
-
-    title = models.CharField(max_length=200)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE) # Категория
-    identification_number = models.AutoField(primary_key=True)
-    unit_of_measurement = models.CharField(max_length=10, default="литр")
-    quantity = models.IntegerField(default=1)
-    price = models.IntegerField()
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='НОРМА')
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Наименование',
+    )
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.CASCADE,
+        verbose_name='Категория',
+    ) # Категория
+    identification_number = models.AutoField(
+        primary_key=True,
+        verbose_name='Идентификационный номер'
+    )
+    unit_of_measurement = models.CharField(
+        max_length=10, 
+        default="литр",
+        verbose_name='Единица измерения'
+    )
+    quantity = models.IntegerField(
+        default=1,
+        verbose_name='Количество'
+    )
+    price = models.IntegerField(
+        verbose_name='Цена',
+    )
+    status = models.CharField(
+        max_length=20, 
+        choices=STATUS_CHOICES, 
+        default='НОРМА'
+    )
 
     def __str__(self):
         return self.title
+
+
+    class Meta:
+        verbose_name = "Архивированный Товар"
+        verbose_name_plural = "Архивированные Товары"
