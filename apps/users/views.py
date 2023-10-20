b46c664a94f17b086f5187877d3eddac41b80f09
GGduisheevArturGG <107888994+ArturDuisheev@users.noreply.github.com>
2023-10-20 17:03:45 +0600

    add comments

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,21 +1,20 @@
 # Import necessary modules
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 from django.contrib.auth import authenticate
 
 # Import serializers (assuming you have created them)
-from .serializers import UserRegistrationSerializer, UserLoginSerializer
+from users import serializers as user_ser
 
 
 # Registration User ViewSet
 class RegistrationUserViewSet(ModelViewSet):
-    serializer_class = UserRegistrationSerializer
+    serializer_class = user_ser.UserRegistrationSerializer
 
     def create(self, request, *args, **kwargs):
-        serializer = UserRegistrationSerializer(data=request.data)
+        serializer = user_ser.UserRegistrationSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
@@ -25,10 +24,10 @@ class RegistrationUserViewSet(ModelViewSet):
 # Login User ViewSet
 # название класса с БОЛЬШОЙ БУКВЫ
 class login_userViewSet(ModelViewSet):
-    serializer_class = UserLoginSerializer
+    serializer_class = user_ser.UserLoginSerializer
 
     def post(self, request, *args, **kwargs):
-        serializer = UserLoginSerializer(data=request.data)
+        serializer = user_ser.UserLoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = authenticate(**serializer.validated_data)
         if user:
