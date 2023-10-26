aea5b19a7f3025710d149d1a1ee97f9a54a74d94
Nurperi <nurperi.back.enova@gmail.com>
2023-10-26 18:56:26 +0600

    refresh token

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     path('login/', views.LoginAPIView.as_view(), name='login'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', views.LogoutAPIView.as_view(), name='logout'),
     path('register/', views.RegistrationView.as_view(), name='register'),
 ]
