71f8737f932efa74829187cf595ad8b80ce981d4
GGduisheevArturGG <107888994+ArturDuisheev@users.noreply.github.com>
2023-11-04 12:39:55 +0600

    add arhcive distrib

--- apps/distributor/views.py
+++ apps/distributor/views.py
@@ -1,3 +1,5 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from . import models as dis_m
@@ -10,8 +12,23 @@ class DistributorViewSet(ModelViewSet):  # GET/PUT/DELETE/CREATE/POST
     serializer_class = ser.DistributorSerializer
     lookup_field = 'pk'
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_archived = True
+        instance.save()
+        Response(status=status.HTTP_200_OK)
+
 
 class ArchivedDistributorView(ModelViewSet):
     queryset = dis_m.Distributor.objects.filter(is_archived=True)
     serializer_class = ser.DistributorSerializer
     lookup_field = 'pk'
+
+    def restore(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_archived = False
+        instance.save()
+
+        return Response(status=status.HTTP_200_OK)
+
+
