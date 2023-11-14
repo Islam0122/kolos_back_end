e51502c993fb9289f489ace07cc200c4312cf543
islam <dujsobaevislam01@gmail.com>
2023-11-14 12:13:55 +0600

    create,update,archive,filter,search -> 100% готов--  2 вариант

--- apps/product/urls.py
+++ apps/product/urls.py
@@ -4,16 +4,21 @@ from .api import views
 urlpatterns = [
     path('db_seed/', views.seeder_start),
     #product
+
+
     path('', views.ProductItemViewSet.as_view({'get': 'list', 'post': 'create'})),
     path('<int:pk>/', views.ProductItemViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
 
     #product -> archive
+
 
     path('archive/', views.ArchivedProductView.as_view({'get': 'list'})),
     path('archive/<int:pk>/', views.ArchivedProductView.as_view({'get': 'retrieve',
                                                                  'put': 'update',
                                                                  'delete': 'restore'})),
     #category
+
+
     path('category/', views.CategoryViewSet.as_view({'get': 'list',
                                                      'post': 'create'})),
     path('category/<int:pk>/', views.CategoryViewSet.as_view({'get': 'retrieve',
