34f4f28698061542dc4c40cc83a22dd4dfbc713a
Alina <gartok.alina@gmail.com>
2023-12-03 01:52:29 +0600

    добавила измение в склады продуктов

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -3,13 +3,10 @@ from django.contrib import admin
 from . import models as m
 
 admin.site.register(m.Category)
+admin.site.register(m.ProductDefect)
 
 
 @admin.register(m.ProductNormal)
 class ProductAdmin(admin.ModelAdmin):
     list_filter = ["is_archived", "state", "category"]
 
-
-@admin.register(m.ProductDefect)
-class ProductAdmin(admin.ModelAdmin):
-    list_filter = ["is_archived", "state", "category"]
--- apps/product/models.py
+++ apps/product/models.py
@@ -4,6 +4,18 @@ from . import choices
 from common.models import BaseModel
 
 
+
+class Warehouse(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name=_('Название склада'))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('Склад')
+        verbose_name_plural = _('Склад')
+
+
 class Category(models.Model):
     title = models.CharField(
         _('Категория'),
@@ -65,6 +77,12 @@ class ProductNormal(BaseModel):
         _('Архив? '),
         default=False
     )
+    warehouse = models.ForeignKey(
+        Warehouse,
+        on_delete=models.CASCADE,
+        related_name='normal_products',
+        verbose_name=_('Склад (нормальный товар)')
+    )
 
     def __str__(self):
         return f'наименование: {self.name}, кол-во: {self.quantity}'
@@ -79,59 +97,14 @@ class ProductNormal(BaseModel):
 
 
 class ProductDefect(BaseModel):
-    name = models.CharField(
-        _('Наименование'),
-        max_length=250
-    )
-    category = models.ForeignKey(
-        Category,
-        on_delete=models.CASCADE,
-        related_name='ProductDefect',
-
-    )
-    identification_number = models.CharField(
-        _('ID'),
-        unique=True,
-        max_length=200,
-
-    )
-    unit = models.CharField(
-        _('Ед.измерения'),
-        max_length=8,
-        choices=choices.Unit.choices,
-        default=choices.Unit.ITEM
-    )
-
-    quantity = models.IntegerField(
-        _('Кол-во'),
-        default=1
-    )
-    price = models.IntegerField(
-        _('Цена'),
-    )
-    sum = models.IntegerField(
-        _('Сумма'),
-        default=0
-    )
-    state = models.CharField(
-        _('Состояние'),
-        max_length=20,
-        choices=choices.State.choices,
-        default=choices.State.NORMAL
-    )
-
-    is_archived = models.BooleanField(
-        _('Архив? '),
-        default=False
-    )
+    product = models.ForeignKey(ProductNormal, on_delete=models.CASCADE, related_name='defective_products',
+                                verbose_name='Бракованный товар')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Колличество')
 
     def __str__(self):
-        return f'наименование: {self.name}, кол-во: {self.quantity}'
-
-    def save(self, *args, **kwargs):
-        self.sum = self.quantity * self.price
-        return super().save(*args, **kwargs)
+        return f"DefectiveProduct {self.product.name}"
 
     class Meta:
-        verbose_name = _('Товар')
-        verbose_name_plural = _('Склад-Брак-Товаров')
+        verbose_name = _('Бракованные товары')
+        verbose_name_plural = _('Бракованные товары')
+
