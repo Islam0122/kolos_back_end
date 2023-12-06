ddbeca854a174a5c6d90f700f66c841c379da1c6
ARDAGER <abdyrakhmanov.arzybek@gmail.com>
2023-12-06 20:35:15 +0600

    update acces token life

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -6,7 +6,7 @@ from .views import TestEndpoint
 urlpatterns = [
     path('login/', views.LoginAPIView.as_view(), name='login'),
     # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', views.LogoutAPIView.as_view(), name='logout'),
     # path('register/', views.RegistrationView.as_view(), name='register'),
     # path('test/', TestEndpoint.as_view(), name='test_endpoint'),
